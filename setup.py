219e0f3a11fb70ff2e13fb451c21587330b4ebe5
SirPythonPhoenix <emil.alexander.vinu@gmail.com>
2023-08-28 11:19:43 +0200

    changed setup.py

--- setup.py
+++ setup.py
@@ -3,14 +3,14 @@ from distutils.core import setup
 setup(
     name='EmilsNaiveBayes',  # How you named your package folder (MyLib)
     packages=['EmilsNaiveBayes'],  # Chose the same as "name"
-    version='0.2',  # Start with a small number and increase it with every change you make
+    version='0.3',  # Start with a small number and increase it with every change you make
     license='MIT',  # Chose a license from here: https://help.github.com/articles/licensing-a-repository
     description='Naive Bayes Algorithm implemented by me.',  # Give a short description about your library
     author='Emil Vinu',  # Type in your name
     author_email='emil.alexander.vinu@gmail.com',  # Type in your E-Mail
     url='https://github.com/SirPythonPhoenix/NaiveBayesPackage',
     # Provide either the link to your gitHub or to your website
-    download_url='https://github.com/SirPythonPhoenix/NaiveBayesPackage/archive/refs/tags/v_0.2.tar.gz',
+    download_url='https://github.com/SirPythonPhoenix/NaiveBayesPackage/archive/refs/tags/v_0.3.tar.gz',
     # I explain this later on
     keywords=['Naive Bayes', 'Machine Learning'],  # Keywords that define your package best
     install_requires=[],
